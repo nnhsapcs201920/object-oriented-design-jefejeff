93a4cabc4e6b5d5a312c0859262bfbe20501e672
jefejeff <54154830+jefejeff@users.noreply.github.com>
2020-01-28 10:30:17 -0600

    Started ComboLock

---
+++ Class Notes/ComboLock.java
@@ -0,0 +1,68 @@
+/**
+ * A class that models a combination lock
+ *
+ * @author jcheng3
+ * @version 28 January 2020
+ */
+public class ComboLock
+{
+    private int first;
+    private int second;
+    private int third;
+    private int lockNumber = 0;
+    private boolean firstCheck = false;
+    private boolean secondCheck = false;
+    private boolean thirdCheck = false;
+
+    /**
+     * Constructor for objects of class ComboLock
+     */
+    public ComboLock(int secret1, int secret2, int secret3)
+    {
+        first = secret1;
+        second = secret2;
+        third = secret3;
+    }
+
+    /**
+     * A method to reset the lock to 0
+     */
+    public void reset()
+    {
+        lockNumber = 0;
+    }
+
+    /**
+     * A method to turn the lock clockwise
+     */
+    public void turnClockwise(int ticks)
+    {
+        for(int i = 0; i <ticks; i++)
+        {
+            lockNumber--;
+            if(lockNumber == 0)
+            {
+                lockNumber = 40;
+            }
+        }
+    }
+
+    /**
+     * A method to turn the lock counterclockwise
+     */
+    public void turnCounterClockwise(int ticks)
+    {
+        for(int i = 0; i <ticks; i++)
+        {
+            lockNumber++;
+            if(lockNumber == 40)
+            {
+                lockNumber = 0;
+            }
+        }
+        if(lockNumber == second)
+        {
+            secondCheck = true;
+        }
+    }
+}
