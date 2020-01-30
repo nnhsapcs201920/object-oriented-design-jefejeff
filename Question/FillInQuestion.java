c1935c3d5b6d65f0308c78e6fb00d8818a2a83ae
jefejeff <54154830+jefejeff@users.noreply.github.com>
2020-01-30 10:30:06 -0600

    ch 9 notes

---
+++ Question/FillInQuestion.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * A FillInQuestion is constructed with a strong that contains the answer surrounede by '_'.
+ *      For example, "The inventor of Java is _James Gosling_." The question should be dispalyed as: The inventor of Java is _____.
+ * @author jcheng3
+ * @version 30 January 2020
+ */
+
+/*
+ * The FillInQuestion class extends (i.e., is a subclass of) the Question class.
+ */
+public class FillInQuestion extends Question
+{
+    /*
+     * Do not declare instance variables for text and answer! The text and answer intance variables are inherited from the Quesiton class!
+     */
+}
