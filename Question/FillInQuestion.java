8533396dd102d4bb9d80c9dd97ce645f5fdbe782
jefejeff <54154830+jefejeff@users.noreply.github.com>
2020-01-31 10:29:12 -0600

    NOTES

--- Question/FillInQuestion.java
+++ Question/FillInQuestion.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 /**
- * A FillInQuestion is constructed with a strong that contains the answer surrounede by '_'.
+ * A FillInQuestion is constructed with a strong that contains the answer surrounded by '_'.
  *      For example, "The inventor of Java is _James Gosling_." The question should be dispalyed as: The inventor of Java is _____.
  * @author jcheng3
  * @version 30 January 2020
@@ -13,6 +13,24 @@ import java.util.Scanner;
 public class FillInQuestion extends Question
 {
     /*
-     * Do not declare instance variables for text and answer! The text and answer intance variables are inherited from the Quesiton class!
+     * Do not declare instance variables for text and answer! The text and answer intance variables are inherited from the Question class!
      */
+
+    /**
+     * Constructs a FillInQuestion object with the specified text that contains the answer
+     * 
+     * @param   question        the specified question text with the embedded answer
+     */
+    public FillInQuestion(String question)
+    {
+        /*
+         * Explicitly call the Question class's constructor that takes a string parameter
+         *      Calling a superclass's constructor must be the first line of code in the
+         *      subclass's constructor.
+         *      
+         * If we don't explicitly call a superclass's constructor, Java will automatically call the
+         *      superclass's default (i.e., no parameters) constructor.
+         */
+        super(question);        
+    }    
 }
