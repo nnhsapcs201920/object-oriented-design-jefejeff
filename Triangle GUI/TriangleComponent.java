1e6250addf3048cb30b669a798427f096310500e
jefejeff <54154830+jefejeff@users.noreply.github.com>
2020-02-18 13:26:10 -0600

    attempted to finish triangle gui, catch up with class

--- Triangle GUI/TriangleComponent.java
+++ Triangle GUI/TriangleComponent.java
@@ -10,10 +10,21 @@ import java.awt.event.MouseListener;
 public class TriangleComponent extends JComponent
 {
     private static final int MAX_POINTS = 3;
+    private int clickCount = 0;
+    private Point2D.Double point1;
+    private Ellipse2D.Double circle;
+    private Point2D.Double point2;
+    private Line2D.Double line1;
+    private Point2D.Double point3;
+    private Line2D.Double line2;
+    private Line2D.Double line3;
+    private int mouseX;
+    private int mouseY;
 
     public TriangleComponent()
     {
-
+        TriangleListener listener = new TriangleListener();
+        this.addMouseListener(listener);
     }
 
     @Override
@@ -21,7 +32,6 @@ public class TriangleComponent extends JComponent
     {  
         super.paintComponent( g );
         Graphics2D g2 = (Graphics2D) g;
-
     }
 
     public class TriangleListener implements MouseListener
@@ -29,11 +39,44 @@ public class TriangleComponent extends JComponent
         @Override
         public void mouseClicked(MouseEvent e)
         {
+            mouseX = e.getX();
+            mouseY = e.getY();
+            System.out.println("clicked!");
+            clickCount++;
+            if(clickCount == 1)
+            {
+                circle = new Ellipse2D.Double(mouseX - 5, mouseY - 5, 10, 10);
+                point1 = new Point2D.Double(mouseX, mouseY);
+            }
+            else if (clickCount == 2)
+            {
+                point2 = new Point2D.Double(mouseX, mouseY);
+                circle = null;
+                line1 = new Line2D.Double(point1.getX(), point1.getY(), point2.getX(), point2.getY());
+            }
+            else if (clickCount == 3)
+            {
+                point3 = new Point2D.Double(mouseX, mouseY);
+                line2 = new Line2D.Double(point1.getX(), point1.getY(), point3.getX(), point3.getY());
+                line2 = new Line2D.Double(point1.getX(), point1.getY(), point3.getX(), point3.getY());
+            }
+            else
+            {
+                point1 = null;
+                point2 = null;
+                point3 = null;
+                line1 = null;
+                line2 = null;
+                line3 = null;
+                clickCount = 0;
+            }
+
         }
 
         @Override
         public void mousePressed(MouseEvent e)
         {
+
         }
 
         @Override
